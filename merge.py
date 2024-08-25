cabf7acf32d6c633c7a95fcc5e9fd22a15275f72
Jignacio14 <j14castro2001@gmail.com>
2024-08-25 15:47:48 -0300

    Genero mas conflicto

--- merge.py
+++ merge.py
@@ -2,6 +2,9 @@ def main():
     print("Hola mundo")
     return 0
 
+def imprimirCosasRandom():
+    print("Quiero crear un conflicto")
+
 def factorial(n: int):
     if n <= 0 or n == 1:
         return 1
