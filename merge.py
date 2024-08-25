9a23d04077873bb9b2e21ac9d05f7c1257337d42
Jignacio14 <j14castro2001@gmail.com>
2024-08-25 15:44:09 -0300

    Mas cambios para generar conflicto

--- merge.py
+++ merge.py
@@ -2,6 +2,12 @@ def main():
     print("Hola mundo")
     return 0
 
+def for_dinamico(n: int):
+    for i in range(n): 
+        print(n)
+
+
+
 def factorial(numero: int):
     if numero <= 1: 
         return 1
